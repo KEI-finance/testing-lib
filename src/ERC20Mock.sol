528ab9210d9aa33ef4f91d24b2a84790690d72a1
Kevin Upton <k_upton@live.com>
2024-02-28 03:00:32 -0600

    fixed tests

--- src/ERC20Mock.sol
+++ src/ERC20Mock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract ERC20Mock is ERC20Burnable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
