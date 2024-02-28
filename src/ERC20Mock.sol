362cb4b9a69091d40f49c5f7886f4cabc5f1370a
Kevin Upton <k_upton@live.com>
2024-02-28 03:00:11 -0600

    updated burn method

--- src/ERC20Mock.sol
+++ src/ERC20Mock.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ERC20Mock is ERC20 {
+contract ERC20Mock is ERC20Burnable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     function mint(address account, uint256 amount) external {
