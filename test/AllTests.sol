68517b8044f62163ff6450dd49fc97a76ec1d6b2
Kevin Upton <k_upton@live.com>
2024-07-23 08:41:55 +1000

    added ERC1820Test

--- test/AllTests.sol
+++ test/AllTests.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "src/UniswapTest.sol";
+import {UniswapTest} from "src/UniswapTest.sol";
+import {ERC1820Test} from "src/ERC1820Test.sol";
 
-contract AllTests is UniswapTest {
-    function test_create() external {}
+contract AllTests is UniswapTest, ERC1820Test {
+    function test_create() external {
+        assertGt(ERC1820Address.code.length, 0);
+    }
 }
